e130f50970f00132d9c25b582d1131bb0c5aa5f9
Josse <josse@shellback.nl>
2022-09-10 11:08:28 +0200

    Add project files.

---
+++ FollowService/Controllers/FollowerController.cs
@@ -0,0 +1,59 @@
+using Amazon.DynamoDBv2.DataModel;
+using FollowerService.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DynamoStudentManager.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FollowerController : ControllerBase
+{
+    private readonly IDynamoDBContext _context;
+
+    public FollowerController(IDynamoDBContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("{userId}")]
+    public async Task<IActionResult> GetById(int userId)
+    {
+        var user = await _context.LoadAsync<Follower>(userId);
+        if (user == null) return NotFound();
+        return Ok(user);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllFollowers()
+    {
+        var followers = await _context.ScanAsync<Follower>(default).GetRemainingAsync();
+        return Ok(followers);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateFollower(Follower followerRequest)
+    {
+        var follower = await _context.LoadAsync<Follower>(followerRequest.Id);
+        if (follower != null) return BadRequest($"Follower with Id {followerRequest.Id} Already Exists");
+        await _context.SaveAsync(followerRequest);
+        return Ok(followerRequest);
+    }
+
+    [HttpDelete("{followerId}")]
+    public async Task<IActionResult> DeleteFollower(int followerId)
+    {
+        var follower = await _context.LoadAsync<Follower>(followerId);
+        if (follower == null) return NotFound();
+        await _context.DeleteAsync(follower);
+        return NoContent();
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> UpdateFollower(Follower followerRequest)
+    {
+        var follower = await _context.LoadAsync<Follower>(followerRequest.Id);
+        if (follower == null) return NotFound();
+        await _context.SaveAsync(followerRequest);
+        return Ok(followerRequest);
+    }
+}
---
+++ FollowService/Models/Follower.cs
@@ -0,0 +1,18 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace FollowerService.Models
+{
+    [DynamoDBTable("follower")]
+    public class Follower
+    {
+        [DynamoDBHashKey("id")]
+        public int? Id { get; set; }
+
+        [DynamoDBProperty("userId")]
+        public int? UserId { get; set; }
+
+        [DynamoDBProperty("followerUserId")]
+        public int? FollowerUserId { get; set; }
+
+    }
+}
